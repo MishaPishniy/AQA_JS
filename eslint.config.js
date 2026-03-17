6174f8c1d77316bb554e821ed35ac6a1b585cf0c
MishaP <MishaP635@gmail.com>
2026-03-17 20:32:32 +0200

    les12

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	// matches all files ending with .js
+	{
+		files: ["**/*.js"],
+		rules: {
+      "no-unused-vars": "error"
+		},
+	},
+
+
+]);
---
+++ les12/les12.js
@@ -0,0 +1,22 @@
+const arr = ['asa', aasa];
+let a = true;
+if (a) {
+	console.log(TRUE);
+}
+
+const obj = {
+	name: 'alice',
+	age: 19,
+	color: 'blue',
+	ages: 25,
+	col: red,
+	fast: true,
+	layzy: false
+};
+
+function newSerkrfmemlwecsacas(
+	param1,
+	pamw2,
+	param3,
+	param4t
+) {}
